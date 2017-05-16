569d67dd7e2c39e4e14592d0f75da13b5b307b61
Jessica Schultz <jessicaschultz1203@gmail.com>
2017-05-16 16:06:21 -0700

    List chat rooms (ckpt. 2) almost completed. needs to still fix  reference err on HomeCtrl.js

--- app/scripts/controllers/HomeCtrl.js
+++ app/scripts/controllers/HomeCtrl.js
@@ -1,8 +1,9 @@
 (function() {
-    function HomeCtrl() {
+    function HomeCtrl(Room) {
+        $scope.rooms = rooms;
     }
   
     angular
         .module('blocChat')
-        .controller('HomeCtrl', [HomeCtrl]);
+        .controller('HomeCtrl', ['Room','$scope', HomeCtrl]);
 })();
