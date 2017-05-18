c676c4a39eaca07dcee13fb61c9d54ed909b365c
Jessica Schultz <jessicaschultz1203@gmail.com>
2017-05-18 16:18:15 -0700

    List chat rooms completed

--- app/scripts/services/Rooms.js
+++ app/scripts/services/Rooms.js
@@ -7,9 +7,16 @@
         Room.all = rooms;
       
         return Room;
+      
+      console.log(Room);
     }
   
     angular
         .module('blocChat')
         .factory('Room', ['$firebaseArray', Room]);
 })();
+
+/* How could I test to see what the value of above? I tried console.log, and calling Room(), etc. in the console and couldn't get anything to show up.
+
+'rooms'/ 'Room' issue...Do i have to keep 'rooms' or 'Room' name because of how i have it in Firebase Database?
+*/
