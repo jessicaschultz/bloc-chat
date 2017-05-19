28a7cf1c8318eb6134bd8915818086f18671f36c
Jessica Schultz <jessicaschultz1203@gmail.com>
2017-05-18 17:29:13 -0700

    cleaned up notes

--- app/scripts/controllers/HomeCtrl.js
+++ app/scripts/controllers/HomeCtrl.js
@@ -1,17 +1,9 @@
 (function() {
     function HomeCtrl(Room) {
-      //$scope.Rooms = rooms;
       this.rooms = Room.all;
-
-      console.log(this.rooms);
-      
     }
   
     angular
         .module('blocChat')
         .controller('HomeCtrl', ['Room', HomeCtrl]);
 })();
-
-/*Direction: Create a controller and associate it with the home template in a $state. Inject the Room service so that you can assign the array of objects retrieved by the all method to a $scope variable. Display the rooms in the template using ng-repeat.
-
-my first thought was use $scope.Rooms = rooms; but keep getting an error that $scope was undefined. is there a way for $scope to work? How do you know when to use $scope vs. this? */
